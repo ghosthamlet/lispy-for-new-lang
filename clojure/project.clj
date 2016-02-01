e5f19f05e2d1457a353aa2a535aa8157b9a31194
hamlet <gvvvv@163.com>
2016-02-01 20:45:46 +0800

    update clojure jar version

--- clojure/project.clj
+++ clojure/project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/ghosthamlet/lispy-for-new-lang/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/algo.generic "0.1.2"]]
   :repl-options {:init-ns lispy.core})
